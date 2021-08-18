94c28050c8fbfdf5b977c99ffd476ab823cf3780
Marcus Da Coregio <marcusdacoregio@gmail.com>
2021-08-18 15:50:26 -0300

    Step 4 - DSL Authorization rules

--- flights-api/src/main/java/com/example/api/SecurityConfiguration.java
+++ flights-api/src/main/java/com/example/api/SecurityConfiguration.java
@@ -2,27 +2,47 @@ package com.example.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
 	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeHttpRequests((authz) -> authz
+				.antMatchers("/flights/all").hasAuthority("flights:all")
+				.antMatchers("/flights/*/take-off").hasAuthority("flights:approve")
+				.antMatchers("/flights").hasAuthority("flights:read")
+				.anyRequest().hasAuthority("flights:write")
+			)
+			.httpBasic(Customizer.withDefaults())
+			.cors(Customizer.withDefaults());
+		// @formatter:on
+		return http.build();
+	}
+
+	@Bean
 	public UserDetailsService userDetailsService() {
 		UserDetails josh = User.withUsername("josh")
 				.password("{bcrypt}$2a$10$l4d14E8Vzm/xpOKSNi5T..pzRYzMKNN3Fxt0DNkNWZpK/eNsV86ca")
-				.roles("USER")
+				.authorities("flights:all", "flights:approve", "flights:read", "flights:write")
 				.build();
 		UserDetails marcus = User.withUsername("marcus")
 				.password("{bcrypt}$2a$10$l4d14E8Vzm/xpOKSNi5T..pzRYzMKNN3Fxt0DNkNWZpK/eNsV86ca")
-				.roles("USER")
+				.authorities("flights:read", "flights:write")
 				.build();
 		UserDetails steve = User.withUsername("steve")
 				.password("{bcrypt}$2a$10$pSOEyeq4mL6bBPAyLr1JTeitMie5NCsSO9NliAWSDdih04eC3F9IG")
-				.roles("USER")
+				.authorities("flights:read", "flights:write")
 				.build();
 		return new InMemoryUserDetailsManager(josh, marcus, steve);
 	}
