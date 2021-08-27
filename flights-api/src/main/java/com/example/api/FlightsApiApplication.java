304d7d8b34754332469fd3d65d13234c4c950695
Josh Cummings <josh.cummings@gmail.com>
2021-08-27 09:08:39 -0600

    Step 0 - Native Build

--- flights-api/src/main/java/com/example/api/FlightsApiApplication.java
+++ flights-api/src/main/java/com/example/api/FlightsApiApplication.java
@@ -3,11 +3,18 @@ package com.example.api;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.nativex.hint.TypeHints;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement(order = -1)
+@TypeHints({
+		@TypeHint(typeNames = "org.springframework.security.access.expression.method.MethodSecurityExpressionRoot", access = AccessBits.ALL),
+		@TypeHint(typeNames = "org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken", access = AccessBits.ALL)
+})
 public class FlightsApiApplication {
 
 	public static void main(String[] args) {
