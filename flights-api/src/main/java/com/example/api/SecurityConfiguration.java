e21a28c78fb5de0076f7b9b671caea9caa15e771
Josh Cummings <josh.cummings@gmail.com>
2021-08-27 09:58:00 -0600

    Step 11 - Map Scopes to Authorities

--- flights-api/src/main/java/com/example/api/SecurityConfiguration.java
+++ flights-api/src/main/java/com/example/api/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.example.api;
 
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -37,6 +42,29 @@ public class SecurityConfiguration {
 		config.setAllowCredentials(true);
 		source.registerCorsConfiguration("/**", config);
 		return source;
+	}
+
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		authoritiesConverter.setAuthorityPrefix("");
+
+		JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+		authenticationConverter.setJwtGrantedAuthoritiesConverter((jwt) -> {
+			if (!"josh".equals(jwt.getSubject())) {
+				return authoritiesConverter.convert(jwt);
+			}
+			Set<String> authorities = AuthorityUtils.authorityListToSet(authoritiesConverter.convert(jwt));
+			if (authorities.contains("flights:write")) {
+				authorities.add("flights:approve");
+			}
+			if (authorities.contains("flights:read")) {
+				authorities.add("flights:all");
+			}
+			return AuthorityUtils.createAuthorityList(authorities.toArray(String[]::new));
+		});
+
+		return authenticationConverter;
 	}
 
 }
